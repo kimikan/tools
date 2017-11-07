4cb889dd1f098b66b9fc9a13c2ebe927243d49f7
Kimi Kan <k32459871@126.com>
2017-11-07 11:21:10 +0800

    Update main.go

--- tcp_proxy/main.go
+++ tcp_proxy/main.go
@@ -1,54 +1,53 @@
 package main
 
 import (
-	"bufio"
+	"encoding/xml"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"net"
-	"os"
-	"sync"
+	"strconv"
 )
 
-var lock sync.Mutex
-var trueList []string
-var ip string
-
-func main() {
-	ip = ":9999"
-
-	file, err := os.Open("app.def")
-	if err != nil {
-		return
-	}
-	defer file.Close()
-	rb := bufio.NewReader(file)
-
-	trueList = []string{}
-	for {
-		line, _, err := rb.ReadLine()
-
-		if err == io.EOF {
-			break
-		}
-
-		trueList = append(trueList, string(line))
-		//do something fmt.Println(string(line))
-	}
-
-	if len(trueList) <= 0 {
-		fmt.Println("no target address")
-		os.Exit(1)
-	}
-	server()
+type config struct {
+	TargetAddr string `xml:"target_addr"`
+	LocalPort  int    `xml:"local_port"`
 }
 
-func server() {
-	lis, err := net.Listen("tcp", ip)
+func loadConfig() *config {
+	content, err := ioutil.ReadFile("app.def")
+	if err != nil {
+		return nil
+	}
+
+	c := new(config)
+	err = xml.Unmarshal(content, c)
+	if err != nil {
+		return nil
+	}
+
+	return c
+}
+
+func main() {
+	cfg := loadConfig()
+	if cfg == nil {
+		fmt.Println("app.def error")
+		return
+	}
+
+	server(cfg.TargetAddr, cfg.LocalPort)
+}
+
+func server(targetAddr string, port int) {
+	localAddr := ":" + strconv.Itoa(port)
+	lis, err := net.Listen("tcp", localAddr)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 	defer lis.Close()
+
 	for {
 		conn, err := lis.Accept()
 		if err != nil {
@@ -56,44 +55,36 @@ func server() {
 			continue
 		}
 		fmt.Println(conn.RemoteAddr(), conn.LocalAddr())
-		go handle(conn)
+		go handle(conn, targetAddr)
 	}
 }
 
-func handle(sconn net.Conn) {
+func handle(sconn net.Conn, targetAddr string) {
 	defer sconn.Close()
-	ip, ok := getIP()
-	if !ok {
-		return
-	}
-	dconn, err := net.Dial("tcp", ip)
+
+	dconn, err := net.Dial("tcp", targetAddr)
 	if err != nil {
-		fmt.Println("Connect failed: ", ip, err)
+		fmt.Println("Connect failed: ", targetAddr, err)
 		return
 	}
+
 	ExitChan := make(chan bool, 1)
 	go func(sconn net.Conn, dconn net.Conn, Exit chan bool) {
 		_, err := io.Copy(dconn, sconn)
-		fmt.Println("copy failed:", ip, err)
+		if err != nil {
+			fmt.Println("copy failed:", targetAddr, err)
+		}
 		ExitChan <- true
 	}(sconn, dconn, ExitChan)
+
 	go func(sconn net.Conn, dconn net.Conn, Exit chan bool) {
 		_, err := io.Copy(sconn, dconn)
-		fmt.Println("Copy failed : ", ip, err)
+		if err != nil {
+			fmt.Println("Copy failed : ", targetAddr, err)
+		}
 		ExitChan <- true
 	}(sconn, dconn, ExitChan)
+
 	<-ExitChan
 	dconn.Close()
-}
-
-func getIP() (string, bool) {
-	lock.Lock()
-	defer lock.Unlock()
-
-	if len(trueList) < 1 {
-		return "", false
-	}
-	ip := trueList[0]
-	trueList = append(trueList[1:], ip)
-	return ip, true
 }
