b9659167f68d6c2b6e8faf44ef072c3d85c5c6d6
Kimi <27750364+kimikan@users.noreply.github.com>
2018-01-17 16:42:06 +0800

    sz binary broadcast

--- tcp_broadcast/main.go
+++ tcp_broadcast/main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"container/list"
+	"encoding/binary"
 	"encoding/xml"
 	"fmt"
-	"io"
 	"io/ioutil"
 	"net"
 	"strconv"
+	"sync"
 )
 
 type config struct {
@@ -29,22 +31,41 @@ func loadConfig() *config {
 	return c
 }
 
-func main() {
-	cfg := loadConfig()
-	if cfg == nil {
-		fmt.Println("app.def error")
-		return
-	}
+//Server indicats some info
+type Server struct {
+	sync.RWMutex
+	_targetWriteChan chan []byte
+	_sourceWriteChan chan []byte
 
-	server(cfg.TargetAddr, cfg.LocalPort)
+	_targetConn net.Conn
+	_clients    *list.List
+
+	_config *config
 }
 
-func server(targetAddr string, port int) {
-	localAddr := ":" + strconv.Itoa(port)
+func newServer(cfg *config) *Server {
+
+	return &Server{
+		_clients: list.New(),
+		_config:  cfg,
+
+		_sourceWriteChan: make(chan []byte),
+		_targetWriteChan: make(chan []byte),
+	}
+}
+
+func (p *Server) close() {
+	//never close
+	p.clear()
+}
+
+func (p *Server) run() error {
+
+	localAddr := ":" + strconv.Itoa(p._config.LocalPort)
 	lis, err := net.Listen("tcp", localAddr)
 	if err != nil {
 		fmt.Println(err)
-		return
+		return err
 	}
 	defer lis.Close()
 
@@ -55,36 +76,209 @@ func server(targetAddr string, port int) {
 			continue
 		}
 		fmt.Println(conn.RemoteAddr(), conn.LocalAddr())
-		go handle(conn, targetAddr)
+		go p.handle(conn)
 	}
+	//return nil
 }
 
-func handle(sconn net.Conn, targetAddr string) {
-	defer sconn.Close()
+func (p *Server) clear() {
+	p.Lock()
 
-	dconn, err := net.Dial("tcp", targetAddr)
+	for {
+		v := p._clients.Front()
+		if v == nil {
+			break
+		}
+		p._clients.Remove(v)
+
+		if value, ok := v.Value.(net.Conn); ok {
+			value.Close()
+		}
+	}
+	p.Unlock()
+	p._targetConn = nil
+}
+
+func (p *Server) getMessage(conn net.Conn) ([]byte, error) {
+
+	var msgType uint32
+	var bodyLen uint32
+	err := binary.Read(conn, binary.BigEndian, &msgType)
 	if err != nil {
-		fmt.Println("Connect failed: ", targetAddr, err)
+		fmt.Println(err)
+		return nil, err
+	}
+
+	err = binary.Read(conn, binary.BigEndian, &bodyLen)
+	if err != nil {
+		return nil, err
+	}
+
+	var buf []byte
+
+	buf = make([]byte, bodyLen+12)
+	if bodyLen > 0 {
+		buf2 := buf[8:]
+		for {
+			got := 0
+			n, err2 := conn.Read(buf2[got:])
+			if err2 != nil || n < 0 {
+				return nil, err2
+			}
+
+			got += n
+
+			if got < int(bodyLen) {
+				continue
+			}
+
+			break
+		}
+	}
+
+	n, err := conn.Read(buf[bodyLen+8:])
+	if err != nil || n <= 0 {
+		return nil, err
+	}
+
+	binary.BigEndian.PutUint32(buf[:], msgType)
+	binary.BigEndian.PutUint32(buf[4:], bodyLen)
+
+	//fmt.Println(msgType, bodyLen, buf)
+	return buf, nil
+}
+
+func (p *Server) dispatcher() {
+	fmt.Println("dispatcher start")
+	go func() {
+		for {
+			if p._targetConn != nil {
+				buf, err := p.getMessage(p._targetConn)
+				if err != nil {
+					fmt.Println("target conn read: ", err)
+					break
+				}
+
+				if len(buf) > 0 {
+					p._sourceWriteChan <- buf
+					//fmt.Println("target read: ", buf)
+				}
+			}
+		}
+
+		if p._targetConn != nil {
+			p._targetConn.Close()
+			p._targetConn = nil
+		}
+		p.clear()
+		fmt.Println("dispatcher stop")
+	}()
+
+	for {
+		select {
+		case v, ok := <-p._sourceWriteChan:
+
+			if ok {
+				cs := []*list.Element{}
+				p.RLock()
+				for e := p._clients.Front(); e != nil; e = e.Next() {
+					if value, ok := e.Value.(net.Conn); ok {
+						_, err := value.Write(v)
+						if err != nil {
+							cs = append(cs, e)
+							fmt.Println("write failed: ", err)
+							value.Close()
+						}
+					} else {
+						cs = append(cs, e)
+					}
+				}
+				p.RUnlock()
+
+				p.Lock()
+				for _, v := range cs {
+					p._clients.Remove(v)
+				}
+				p.Unlock()
+			}
+		case v, ok := <-p._targetWriteChan:
+			if ok {
+				if p._targetConn != nil {
+					_, err := p._targetConn.Write(v)
+					if err != nil {
+						p.clear()
+						break
+					}
+				} else {
+					break
+				}
+			}
+		} //end select
+	} //end for
+}
+
+func (p *Server) handle(conn net.Conn) {
+
+	if p._targetConn == nil {
+		dconn, err := net.Dial("tcp", p._config.TargetAddr)
+		if err != nil {
+			fmt.Println("Connect target failed: ", p._config.TargetAddr, err)
+			return
+		}
+
+		p._targetConn = dconn
+		go p.dispatcher()
+	}
+
+	go func() {
+		defer conn.Close()
+
+		p.Lock()
+		element := p._clients.PushBack(conn)
+		p.Unlock()
+
+		for {
+			buf, err := p.getMessage(conn)
+			if err != nil {
+				break
+			}
+			fmt.Println("conn read: ", buf)
+
+			if len(buf) > 0 {
+				p.RLock()
+				v := p._clients.Front()
+				if v != nil {
+					if v.Value == conn {
+						p._targetWriteChan <- buf
+					}
+				}
+				p.RUnlock()
+
+			} else {
+				fmt.Println("read 0 buffer")
+				break
+			}
+		}
+		fmt.Println("conn closed")
+		p.Lock()
+
+		p._clients.Remove(element)
+		p.Unlock()
+	}()
+}
+
+func main() {
+	cfg := loadConfig()
+	if cfg == nil {
+		fmt.Println("app.def error")
 		return
 	}
 
-	ExitChan := make(chan bool, 1)
-	go func(sconn net.Conn, dconn net.Conn, Exit chan bool) {
-		_, err := io.Copy(dconn, sconn)
-		if err != nil {
-			fmt.Println("copy failed:", targetAddr, err)
-		}
-		ExitChan <- true
-	}(sconn, dconn, ExitChan)
+	server := newServer(cfg)
+	defer server.close()
 
-	go func(sconn net.Conn, dconn net.Conn, Exit chan bool) {
-		_, err := io.Copy(sconn, dconn)
-		if err != nil {
-			fmt.Println("Copy failed : ", targetAddr, err)
-		}
-		ExitChan <- true
-	}(sconn, dconn, ExitChan)
-
-	<-ExitChan
-	dconn.Close()
+	err := server.run()
+	if err != nil {
+		fmt.Println("Run failed: ", err)
+	}
 }
